adcf88aee5417fbc89b23d70091dd0e43ac5943c
damiantopaz <damiantopaz@gmail.com>
2020-09-15 16:16:38 +0100

    stage-2

---
+++ system/storage/cache/68/680c89f7655e4f67bc169d6b6718153ca4ff7c36992014d0a6996ff8fdf723ab.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_76bc603302194767541bbe11d0b90582101f15d6c3982c908177e79eaef5f661 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
